966e232642c866d26626e09bf19071145c11d4b6
Seetharam <sadalaseetharam@gmail.com>
2023-01-06 17:19:06 +0530

    End Of Section 3

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository,IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await regionRepository.GetAllAsync();
+
+            /*return DTO regions
+
+            var regionsDTO = new List<Models.DTO.Region>();
+            regions.ToList().ForEach(regions =>
+            {
+                var regionDTO = new Models.DTO.Region()
+                {
+                    Id = regions.Id,
+                    Name = regions.Name,
+                    Code = regions.Code,
+                    Area = regions.Area,
+                    Lat = regions.Lat,
+                    Long = regions.Long,
+                    Population = regions.Population,
+                };
+                regionsDTO.Add(regionDTO);
+            });*/
+
+            var regionsDTO = mapper.Map<List<Models.Domain.Region>>(regions);
+
+            return Ok(regionsDTO);
+        }
+    }
+}
---
+++ NZWalks.API/Models/DTO/Region.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public Double Area { get; set; }
+        public Double Lat { get; set; }
+        public Double Long { get; set; }
+        public long Population { get; set; }
+    }
+}
