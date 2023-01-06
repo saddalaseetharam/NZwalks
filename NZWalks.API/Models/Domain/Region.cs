5c3773f8cc982e92dbd0fefee8d5e7a5c6d96b10
Seetharam <sadalaseetharam@gmail.com>
2023-01-06 13:05:58 +0530

    End Of Section 2

---
+++ NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public Double Area { get; set; }
+        public Double Lat { get; set; }
+        public Double Long { get; set; }
+        public long Population { get; set; }
+
+        //Navigation Property
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Double Length { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        //Navigation Property
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
